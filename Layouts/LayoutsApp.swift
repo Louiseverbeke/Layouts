49472654cb0b59b3a15d2943d34fc1c85d9af484
Louise Verbeke <louiseverbeke@yahoo.be>
2026-03-02 18:16:56 -0500

    Initial Commit

---
+++ Layouts/LayoutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutsApp.swift
+//  Layouts
+//
+//  Created by Louise Verbeke on 02/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct LayoutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
