7e4311752088e018114b6e73ae7cc27f469e64a8
Louise Verbeke <louiseverbeke@yahoo.be>
2026-03-08 15:26:23 -0400

    after fundamentals layout part 2

--- Layouts/ContentView.swift
+++ Layouts/ContentView.swift
@@ -8,35 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var messageString = "hen the Genius Bar Needs Help, They Call You!"
+    @State private var messageString = "When the Genius Bar Needs Help, They Call You!"
     
     var body: some View {
-        VStack {
-            Text(messageString)
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .minimumScaleFactor(0.5)
-                .multilineTextAlignment(.center)
-                .foregroundStyle(.red)
-                .frame(height:150)
-                .frame(maxWidth: .infinity)
-                .border(.orange, width: 2)
+            
+            VStack {
+                
+                Spacer()
+                
+                Text(messageString)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .minimumScaleFactor(0.5)
+                    .multilineTextAlignment(.center)
+                    .foregroundStyle(.red)
+                    .frame(height:150)
+                    .frame(maxWidth: .infinity)
+                //  .border(.orange, width: 2)
+                    .padding()
+                
+                Spacer()
+                
+                HStack {
+                    Button("Awesome") {
+                        messageString = "You Are Awesome!"
+                    }
+                    
+                    Spacer()
+                    
+                    Button("Great") {
+                        messageString = "You Are Great!"
+                    }
+                }
+                .buttonStyle(.borderedProminent)
+                //            .border(.purple, width: 5)
                 .padding()
-        
-            HStack {
-                Button("Awesome") {
-                    messageString = "You Are Awesome!"
-                }
-                Button("Great") {
-                    messageString = "You Are Great!"
-                }
             }
-            .buttonStyle(.borderedProminent)
-            .border(.orange, width: 2)
         }
     }
-}
-
-#Preview {
-    ContentView()
-}
+    
+    #Preview {
+        ContentView()
+    }
