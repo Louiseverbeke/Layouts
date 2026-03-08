1e4ccb4cd4fb545285bf03470be4c0d2ded7a5d3
Louise Verbeke <louiseverbeke@yahoo.be>
2026-03-08 16:18:36 -0400

    after fundamentals layout part 3

--- Layouts/ContentView.swift
+++ Layouts/ContentView.swift
@@ -11,42 +11,51 @@ struct ContentView: View {
     @State private var messageString = "When the Genius Bar Needs Help, They Call You!"
     
     var body: some View {
-            
-            VStack {
-                
-                Spacer()
-                
-                Text(messageString)
-                    .font(.largeTitle)
-                    .fontWeight(.heavy)
-                    .minimumScaleFactor(0.5)
-                    .multilineTextAlignment(.center)
-                    .foregroundStyle(.red)
-                    .frame(height:150)
-                    .frame(maxWidth: .infinity)
-                //  .border(.orange, width: 2)
-                    .padding()
-                
-                Spacer()
-                
-                HStack {
-                    Button("Awesome") {
-                        messageString = "You Are Awesome!"
-                    }
-                    
-                    Spacer()
-                    
-                    Button("Great") {
-                        messageString = "You Are Great!"
-                    }
-                }
-                .buttonStyle(.borderedProminent)
-                //            .border(.purple, width: 5)
+        
+        VStack {
+            Text("Awesome!")
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundStyle(.white)
                 .padding()
+                .frame(maxWidth: .infinity)
+                .background(.indigo)
+            
+            Spacer()
+            
+            Text(messageString)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+                .foregroundStyle(.red)
+                .frame(height:150)
+                .frame(maxWidth: .infinity)
+                .padding()
+            
+            Spacer()
+            
+            HStack {
+                Button("Awesome") {
+                    messageString = "You Are Awesome!"
+                }
+                
+                Spacer()
+                
+                Button("Great") {
+                    messageString = "You Are Great!"
+                }
             }
+            .buttonStyle(.borderedProminent)
+            .padding()
+            
+            Rectangle()
+                .frame(height: 0)
+                .background(.indigo)
         }
     }
-    
-    #Preview {
-        ContentView()
-    }
+}
+
+#Preview {
+    ContentView()
+}
